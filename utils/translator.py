c1736b76eefef39e1fa69fbdef5594be9dc16312
Ivan Hulin <lugbuisnessman@gmail.com>
2025-09-25 16:55:04 +0300

    CHAT GPT

--- utils/translator.py
+++ utils/translator.py
@@ -1,6 +1,15 @@
+# файл для перевода текста через Google Translate
 from googletrans import Translator
 
 def translate_text(text: str, src: str = "auto", dest: str = "en") -> str:
+    """
+    Переводит текст с языка src на язык dest.
+    
+    :param text: текст для перевода
+    :param src: исходный язык (по умолчанию "auto")
+    :param dest: язык перевода (по умолчанию "en")
+    :return: переведённый текст или сообщение об ошибке
+    """
     translator = Translator()
     try:
         result = translator.translate(text, src=src, dest=dest)
